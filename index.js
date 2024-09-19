bf33b8bfd3bb5c496cca7efe3a31b314fb924bbe
janine pamintuan <jaja.ninnin@gmail.com>
2024-09-19 10:26:18 -0400

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
+function increment(number) {
+  return number+= 1;
+}
+function decrement(number) {
+  return number-= 1;
+}
+function makeInt(number) {
+  return parseInt(number, 10);
+}
+function preserveDecimal(number) {
+  return parseFloat(number);
+}
